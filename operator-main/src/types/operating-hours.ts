3579f052ff37967692a3b1b3cf623b0a4266490c
Kripesh <kg9773092+github@gmail.com>
2026-07-06 07:33:25 +0545

    Removed bogus module

--- operator-main/src/types/operating-hours.ts
+++
@@ -1,7 +0,0 @@
-export interface OperatingHours {
-  type: "24/7" | "custom";
-  openTime?: string; // HH:mm format
-  closeTime?: string; // HH:mm format
-  weekdayHours?: OperatingHours;
-  weekendHours?: OperatingHours;
-}
